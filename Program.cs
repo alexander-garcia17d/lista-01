455259ec52c2a5962a785a3f0b35bfde1e317b65
alexander <alexander_j_garcia@estudante.sesisenai.org.br>
2026-06-15 19:45:24 -0300

    Contador Progresivo

--- Program.cs
+++ Program.cs
@@ -1,25 +1,9 @@
 ﻿using System;
 
-Console.Write("Informe a idade do passageiro: ");
-int idade = int.Parse(Console.ReadLine()!);
+Console.Write("Informe o número final da contagem: ");
+int limite = int.Parse(Console.ReadLine()!);
 
-Console.Write("Informe o valor normal da passagem: ");
-decimal valorNormal = decimal.Parse(Console.ReadLine()!);
-
-decimal valorAPagar;
-
-if (idade <= 5)
+for (int i = 1; i <= limite; i++)
 {
-    valorAPagar = 0m;
+    Console.WriteLine(i);
 }
-else if (idade >= 60)
-{
-    valorAPagar = valorNormal / 2m;
-}
-else
-{
-    valorAPagar = valorNormal;
-}
-
-Console.WriteLine($"Valor normal da passagem: R$ {valorNormal}");
-Console.WriteLine($"Valor a pagar: R$ {valorAPagar}");
