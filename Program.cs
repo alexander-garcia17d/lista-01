f998a2dacb3acb642ab437595b1da039ea5f2199
alexander <alexander_j_garcia@estudante.sesisenai.org.br>
2026-06-15 19:54:30 -0300

    calculador de media

--- Program.cs
+++ Program.cs
@@ -1,11 +1,22 @@
 ﻿using System;
 
-Console.Write("Informe o número da tabuada: ");
-int numero = int.Parse(Console.ReadLine()!);
+Console.Write("Informe o primeiro valor: ");
+decimal valor1 = decimal.Parse(Console.ReadLine()!);
 
-for (int multiplicador = 1; multiplicador <= 10; multiplicador++)
-{
-    int resultado = numero * multiplicador;
-    Console.WriteLine($"{numero} x {multiplicador} = {resultado}");
-}
+Console.Write("Informe o segundo valor: ");
+decimal valor2 = decimal.Parse(Console.ReadLine()!);
 
+Console.Write("Informe o terceiro valor: ");
+decimal valor3 = decimal.Parse(Console.ReadLine()!);
+
+Console.Write("Informe o quarto valor: ");
+decimal valor4 = decimal.Parse(Console.ReadLine()!);
+
+Console.Write("Informe o quinto valor: ");
+decimal valor5 = decimal.Parse(Console.ReadLine()!);
+
+decimal soma = valor1 + valor2 + valor3 + valor4 + valor5;
+decimal media = soma / 5m;
+
+Console.WriteLine($"Soma dos valores: {soma}");
+Console.WriteLine($"Média dos valores: {media}");
