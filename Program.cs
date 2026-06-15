46223d87c7863684321edabe113e5cbb45398317
alexander <alexander_j_garcia@estudante.sesisenai.org.br>
2026-06-15 19:42:28 -0300

    Cálculo de passagem por idade

--- Program.cs
+++ Program.cs
@@ -1,14 +1,25 @@
 ﻿using System;
 
-Console.Write("Informe um número inteiro: ");
-int numero = int.Parse(Console.ReadLine()!);
+Console.Write("Informe a idade do passageiro: ");
+int idade = int.Parse(Console.ReadLine()!);
 
-if (numero % 2 == 0)
+Console.Write("Informe o valor normal da passagem: ");
+decimal valorNormal = decimal.Parse(Console.ReadLine()!);
+
+decimal valorAPagar;
+
+if (idade <= 5)
 {
-    Console.WriteLine("O número informado é par.");
+    valorAPagar = 0m;
+}
+else if (idade >= 60)
+{
+    valorAPagar = valorNormal / 2m;
 }
 else
 {
-    Console.WriteLine("O número informado é ímpar.");
+    valorAPagar = valorNormal;
 }
 
+Console.WriteLine($"Valor normal da passagem: R$ {valorNormal}");
+Console.WriteLine($"Valor a pagar: R$ {valorAPagar}");
