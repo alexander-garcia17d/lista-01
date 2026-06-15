1759c1418b83ccd7b034e7aaf77640d51cf82eef
alexander <alexander_j_garcia@estudante.sesisenai.org.br>
2026-06-15 20:10:40 -0300

    Caixa Eletronico

--- Program.cs
+++ Program.cs
@@ -1,18 +1,47 @@
 ﻿using System;
 
-Console.Write("Informe a quantidade de clientes atendidos: ");
-int quantidade = int.Parse(Console.ReadLine()!);
+Console.Write("Informe o saldo inicial: ");
+decimal saldo = decimal.Parse(Console.ReadLine()!);
 
-int tempoTotal = 0;
-
-for (int i = 0; i < quantidade; i++)
+while (true)
 {
-    Console.Write("Informe o tempo do atendimento: ");
-    int tempo = int.Parse(Console.ReadLine()!);
-    tempoTotal += tempo;
+    Console.WriteLine("Escolha a operação:");
+    Console.WriteLine("1 - Entrada de dinheiro");
+    Console.WriteLine("2 - Saída de dinheiro");
+    Console.WriteLine("3 - Encerrar");
+    Console.Write("Opção: ");
+    int operacao = int.Parse(Console.ReadLine()!);
+
+    if (operacao == 1)
+    {
+        Console.Write("Informe o valor da movimentação: ");
+        decimal valor = decimal.Parse(Console.ReadLine()!);
+        saldo += valor;
+        Console.WriteLine($"Entrada registrada. Saldo atual: R$ {saldo}");
+    }
+    else if (operacao == 2)
+    {
+        Console.Write("Informe o valor da movimentação: ");
+        decimal valor = decimal.Parse(Console.ReadLine()!);
+
+        if (valor <= saldo)
+        {
+            saldo -= valor;
+            Console.WriteLine($"Saída registrada. Saldo atual: R$ {saldo}");
+        }
+        else
+        {
+            Console.WriteLine("Saldo insuficiente.");
+        }
+    }
+    else if (operacao == 3)
+    {
+        Console.WriteLine($"Saldo final: R$ {saldo}");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Operação inválida. Tente novamente.");
+    }
 }
 
-decimal tempoMedio = quantidade > 0 ? (decimal)tempoTotal / quantidade : 0m;
-
-Console.WriteLine($"Tempo total de atendimento: {tempoTotal} minutos");
-Console.WriteLine($"Tempo médio por cliente: {tempoMedio} minutos");
